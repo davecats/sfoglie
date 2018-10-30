2c0cee4ed3c3a8627566035d5522c2a2ea04e02e
Davide Gatti <msc.davide.gatti@gmail.com>
2018-10-30 10:19:10 +0100

    Update main.m

--- main.m
+++ main.m
@@ -1,3 +1,51 @@
+% 
+%      //////  /////  ////   /////   //     //  /////
+%     //   // //    //   // //  //  //     //  //
+%     //     //    //   // //      //     //  //
+%      //   ////  //   // // //// //     //  /////
+%  //   // //    //   // //   // //     //  //
+%   ///// //      ////    ////  ////// //  /////
+%
+%   
+% This program computes the incompressible flow around an airfoil 
+% with and without uniform blowing
+%
+% ------------------------------------------------------------------------------
+% If you use this code and find it helpful please cite
+% 
+% M.Reder, A. Stroh and D. Gatti, "The effect of uniform blowing on the performance 
+% of airfoils: a boundary element method study", Notes on Numerical Fluid 
+% Mechanics and Multidisciplinary Design, (submitted, 2018)
+% ------------------------------------------------------------------------------
+% 
+% Copyright (C) 2018  Dr. Davide Gatti & M. Reder
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
+%
+% Contacts:
+%
+% Dr. Davide Gatti  
+% davide.gatti [at] kit.edu
+% msc.davide.gatti [at] gmail.com
+% 
+% Karlsruhe Institute of Technology
+% Institute of Fluid Dynamics
+% Kaiserstraße 10
+% 76131 Karlsruhe 
+% 
+
+
 init        % initialize: add paths etc.
 parameters  % load parameters for calculation
 % inviscid solution 
